8eb2ea79857409b7753ce24e4e2bd3de2dffad86
manolast <manuel.stapff@gmail.com>
2023-05-31 10:16:12 -0700

    nada roto, faltan cosas pero viene bien

--- js/app.js
+++ js/app.js
@@ -1,88 +1,3 @@
-window.addEventListener("load", inicio);
-function inicio(){
-    const sys = new Sistema();
-    const lupa = document.getElementById("idLupa");
-    const botonAgregar = document.getElementById("idBotonReclamo");
-    const botonAgregarForm = document.getElementById("idBotonAgregar");
-    const principal = document.getElementById("idLinksS1");
-    const verReclamos = document.getElementById("idLinksS2");
-    const estadisticas = document.getElementById("idLinksS3");
-    const seccionAgregar = document.getElementById("idLinksS4");
-
-    // lupa.addEventListener("click", buscar);
-    // botonAgregar.addEventListener("click", nuevoReclamo);
-    botonAgregarForm.addEventListener("click", agregarReclamo);
-    // botonNuevaEmpresa.addEventListener("click", agregarEmp);
-}
-
-function agregarReclamo(){
-    const formRec = document.getElementById("idNuevoForm");
-    if(formRec.reportValidity()){
-        const nombreUsuario = document.getElementById("idNombreUsuario").value;
-        const empresa = document.getElementById("idEmpresa").value;
-        const titulo = document.getElementById("idReclamo").value;
-        const descripcion = document.getElementById("idDescripcion").value;
-        const reclamo0 = new Reclamo(nombreUsuario, empresa, titulo, descripcion);
-        sys.agregarReclamo(reclamo0);
-        const numReclamo = sys.reclamos.indexOf(reclamo0) + 1;
-        crearElementoReclamo(nombreUsuario, titulo, empresa, descripcion, "1")
-    }
-}
-function crearElementoReclamo(nombre, titulo, empresa, descripcion, numero){
-    const divReclamo = document.createElement("div", {
-        class:"containerReclamo"
-    })
-    const contenido = document.createElement("div", {
-        class:"contenidoReclamo"
-    })
-    const heading = document.createElement("h3");
-    const t = document.createTextNode("Reclamo No. " + numero);
-    titulo.appendChild(t);
-    divReclamo.appendChild(heading);
-
-    const pUsuario = document.createElement("p");
-    pUsuario.innerText = nombre + ": ";
-    const spanTitulo = document.createElement("span", {
-        class:"comentUsuario"
-    })
-    spanTitulo.innerText=titulo;
-    pUsuario.appendChild(spanTitulo);
-    const pEmpresa = document.createElement("p");
-    pEmpresa.innerText="Empresa: ";
-    const spanEmpresa = document.createElement("span", {
-        class:nombreEmpresa
-    })
-    spanEmpresa.innerText=empresa;
-    pEmpresa.appendChild(spanEmpresa);
-    const pDescripcion = document.createElement("p");
-    pDescripcion.innerText=descripcion;
-    const buttonTambien = document.createElement("button", {
-        id:numero
-    })
-    const labelContador = document.createElement("label", {
-        for:numero
-    })
-    contenido.appendChild(pUsuario);
-    contenido.appendChild(pEmpresa);
-    contenido.appendChild(pDescripcion);
-    contenido.appendChild(buttonTambien);
-    contenido.appendChild(labelContador);
-    divReclamo.appendChild(contenido);
-    document.getElementById("idSection2").appendChild(divReclamo);
-}
-
-// function agregarEmp(){
-//     const formEmp = document.getElementById("idFormNuevaEmp");
-//     if(formEmp.reportValidity()){
-//         const nombreEmpresa = document.getElementById("idNombreEmpresa").value;
-//         const direccion = document.getElementById("idDireccion").value;
-//         const rubro = document.getElementById("idRubro").value;
-//         const empresa0 = new Empresa(nombreEmpresa, direccion, rubro);
-//         sys.agregarEmpresa(empresa0);
-//     }
-// }
-
-
 class Sistema{
     constructor(){
         this.empresas = [];
@@ -96,11 +11,12 @@ class Sistema{
     }
     buscarReclamo(keyword){
         let resp = [];
-        for(let rec of this.reclamos){
+        for(let i=0; i<this.reclamos.length; i++){
             if(rec.toString().toUpperCase().includes(keyword.toUpperCase())){
-                resp.push(rec);
+                resp.push(i);
             }
         }
+        return resp;
     }
 }
 
@@ -127,4 +43,86 @@ class Empresa{
         this.cantidad = 0;
     }
 }
+
+
+
+window.addEventListener("load", inicio);
+const sys = new Sistema();
+function inicio(){
+    const lupa = document.getElementById("idLupa");
+    const botonAgregar = document.getElementById("idBotonReclamo");
+    const botonAgregarForm = document.getElementById("idBotonAgregar");
+    const principal = document.getElementById("idLinksS1");
+    const verReclamos = document.getElementById("idLinksS2");
+    const estadisticas = document.getElementById("idLinksS3");
+    const seccionAgregar = document.getElementById("idLinksS4");
+    
+    // lupa.addEventListener("click", buscar);
+    // botonAgregar.addEventListener("click", nuevoReclamo);
+    botonAgregarForm.addEventListener("click", agregarReclamo);
+    // botonNuevaEmpresa.addEventListener("click", agregarEmp);
+}
+
+function agregarReclamo(){
+    const formRec = document.getElementById("idNuevoForm");
+    if(formRec.reportValidity()){
+        const nombreUsuario = document.getElementById("idNombreUsuario").value;
+        const empresa = document.getElementById("idEmpresa").value;
+        const titulo = document.getElementById("idReclamo").value;
+        const descripcion = document.getElementById("idDescripcion").value;
+        const reclamo0 = new Reclamo(nombreUsuario, empresa, titulo, descripcion);
+        sys.agregarReclamo(reclamo0);
+        const numReclamo = sys.reclamos.indexOf(reclamo0) + 1;
+        crearElementoReclamo(nombreUsuario, titulo, empresa, descripcion, numReclamo);
+    }
+}
+function crearElementoReclamo(nombre, titulo, empresa, descripcion, numero){
+    const divReclamo = document.createElement("div");
+    divReclamo.setAttribute("class", "containerReclamo");
+    const contenido = document.createElement("div");
+    contenido.setAttribute("class", "reclamo");
+    const heading = document.createElement("h3");
+    const t = document.createTextNode("Reclamo No. " + numero);
+    heading.appendChild(t);
+    divReclamo.appendChild(heading);
+    const pUsuario = document.createElement("p");
+    pUsuario.innerText = nombre + ": ";
+    const spanTitulo = document.createElement("span");
+    spanTitulo.setAttribute("class", "comentUsuario");
+    spanTitulo.innerText=titulo;
+    pUsuario.appendChild(spanTitulo);
+    const pEmpresa = document.createElement("p");
+    pEmpresa.innerText="Empresa: ";
+    const spanEmpresa = document.createElement("span");
+    spanEmpresa.setAttribute("class", "nombreEmpresa");
+    spanEmpresa.innerText=empresa;
+    pEmpresa.appendChild(spanEmpresa);
+    const pDescripcion = document.createElement("p");
+    pDescripcion.innerText=descripcion;
+    const buttonTambien = document.createElement("button");
+    buttonTambien.setAttribute("id", numero);
+    buttonTambien.innerText="¡A mi también me pasó!"
+    const labelContador = document.createElement("label");
+    labelContador.setAttribute("for", numero);
+    labelContador.innerText="Contador: 1";
+    contenido.appendChild(pUsuario);
+    contenido.appendChild(pEmpresa);
+    contenido.appendChild(pDescripcion);
+    contenido.appendChild(buttonTambien);
+    contenido.appendChild(labelContador);
+    divReclamo.appendChild(contenido);
+    document.getElementById("idSection2").appendChild(divReclamo);
+}
+
+function agregarEmp(){
+    const formEmp = document.getElementById("idFormNuevaEmp");
+    if(formEmp.reportValidity()){
+        const nombreEmpresa = document.getElementById("idNombreEmpresa").value;
+        const direccion = document.getElementById("idDireccion").value;
+        const rubro = document.getElementById("idRubro").value;
+        const empresa0 = new Empresa(nombreEmpresa, direccion, rubro);
+        sys.agregarEmpresa(empresa0);
+    }
+}
+
 
