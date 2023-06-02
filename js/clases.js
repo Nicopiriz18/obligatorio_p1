7710717ba0e67d8a2e1c40212fe3e0a60cd8748d
manolast <manuel.stapff@gmail.com>
2023-06-02 13:32:09 -0700

    sape

--- js/clases.js
+++ js/clases.js
@@ -9,11 +9,13 @@ class Sistema{
     agregarReclamo(rec){
         this.reclamos.push(rec);
     }
-    buscarReclamo(keyword){
+    buscarReclamos(keyword){
         let resp = [];
         for(let i=0; i<this.reclamos.length; i++){
-            if(rec.toString().toUpperCase().includes(keyword.toUpperCase())){
-                resp.push(i);
+            const valorABuscarCoincidencias = this.reclamos[i].valoresReclamo();
+            if(valorABuscarCoincidencias.includes(keyword.toUpperCase())){
+                resp.push((i+1));
+                //se pushea i+1 porque en los reclamos el primer reclamo tiene el nro 1 en lugar de 0
             }
         }
         return resp;
@@ -28,8 +30,8 @@ class Reclamo{
         //el contador se incializa en 1 cuando se hace un reclamo
         this.contador = 1;
     }
-    toString(){
-        return(this.nombreUsuario + ": " + this.titulo + "\n" + "empresa: " + this.empresa + "\n" + this.descripcion);
+    valoresReclamo(){
+        return (this.nombreUsuario + ": " + this.titulo + "  " + this.empresa  + this.descripcion).toUpperCase();
     }
 }
 class Empresa{
